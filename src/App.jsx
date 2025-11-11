2ec9322e4279e54af7b6743364b91ad5e94bd411
iyzu-code <iyzufizeyvin@gmail.com>
2025-11-11 14:50:46 +0700

    feat: add module 11 portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import { createRoot } from "react-dom/client"
-// import Order from "./order"
-// import PizzaOfTheDay from "./PizzaOfTheDay"
-// import Header from "./Header"
-// import { CartContext } from "./context"
 import { StrictMode } from "react"
-
 import { RouterProvider, createRouter } from "@tanstack/react-router"
 import { routeTree } from "./routeTree.gen"
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const router = createRouter({ routeTree })
-
 const queryClient = new QueryClient()
 
 const App = () => {
